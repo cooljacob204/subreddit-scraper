2eb7dcd84a3692cae12d7c2740f737f051fbc21f
Jacob Feldman <jacobgfeld@gmail.com>
2018-12-30 16:09:18 -0500

    Clears objects after test finishes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,14 @@ RSpec.configure do |config|
   # some (optional) config here
 
   config.before(:each) do
-    User.all.clear
-    Post.all.clear
-    Subreddit.all.clear
     @user = User.new("cooljacob204")
     @subreddit = Subreddit.new("programmerhumor", "Not everybody understands the humor of programmers.")
     @post = Post.new("Meme", @subreddit, @user)
+  end
+
+  config.after(:each) do
+    User.reset_all
+    Post.reset_all
+    Subreddit.reset_all
   end
 end
