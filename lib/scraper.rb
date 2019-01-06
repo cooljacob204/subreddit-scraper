0433a49c42bd2673dd7bdf379906fda7db2d317c
Jacob Feldman <jacobgfeld@gmail.com>
2019-01-05 19:44:46 -0500

    Changes our scrapper to properly impliment reddit's API using gem redd

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,51 +1,30 @@
 module SubredditScraper
   class Scraper
-    def self.scrapeSubredditFromUrl(url)
+    def self.scrapeSubredditFromName(name)
       begin
-        url = stripUrl(url)
-        page = Nokogiri::HTML(open(url))
-        scrapeSubreddit(page)
-      rescue
-        return false
-      end
+        session = Redd.it(
+          user_agent: 'Redd:RandomBot (by /u/cooljacob204)',
+          client_id: ENV['CLIENT_ID'],
+          secret: ENV['SECRET'] 
+          )
+          subredditToParse = session.subreddit(name)
+        rescue
+          return false
+        end
+        subreddit = Subreddit.find_or_create_by_name_and_description(
+          subredditToParse.title,
+          subredditToParse.description,
+        )
+        subredditToParse.hot.each do |post|
+          Post.new(
+            post.title,
+            subreddit,
+            User.find_or_create_by_name(post.author.name),
+            :date => post.created,
+            :link => post.url
+          )
+        end
       true
-    end
-
-    def self.scrapeSubredditFromFile(path)
-      begin
-        html = File.read(path)
-        page = Nokogiri::HTML(html)
-        scrapeSubreddit(page)
-      rescue
-        return false
-      end
-      true
-    end
-
-    private
-
-    def self.stripUrl(url)
-      strippedUrl = "https://old.reddit.com"
-      strippedUrl << /\/r\/[a-zA-Z0-9\-_]*/.match(url).to_s
-      strippedUrl
-    end
-    
-    def self.scrapeSubreddit(page)
-      subredditName = page.css('h1.redditname').text
-      description = page.css('blockquote').text.strip
-
-      subreddit = Subreddit.find_or_create_by_name_and_description(subredditName, description)
-
-      page.css('.thing').each do |post|
-        next if !post.css('time')[0] # indicates there is an advertisment
-        title = post.css('a.title').text
-        date = post.css('time')[0]['datetime']
-        author = post.css('a.author').text
-        link = post.css('a.title')[0]['href']
-        user = User.find_or_create_by_name(author)
-
-        Post.new(title, subreddit, user, :date => date, :link => link)
-      end
     end
   end
 end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,18 +1,8 @@
 require 'spec_helper'
 
 describe Scraper do
-  it "Intializes from file" do
-    Scraper.scrapeSubredditFromFile('fixtures/ProgrammerHumor.html')
-    expect(Post.count).to equal(27)
-  end
-
-  xit "Initializes from Url" do
-    Scraper.scrapeSubredditFromUrl("reddit.com/r/funny")
+  it "scrapes from a subreddit name" do
+    Scraper.scrapeSubredditFromName("funny")
     expect(Post.count).to be > 20
-  end
-
-  it "scraper returns false on bad url or file" do
-    expect(Scraper.scrapeSubredditFromFile('fixtures/ProgrammerssHumor.html')).to equal(false)
-    expect(Scraper.scrapeSubredditFromFile('google.com')).to equal(false)
   end
 end
