f832f861bbf6c0dba064b8c8062ffeafb7015d38
Jacob Feldman <jacobgfeld@gmail.com>
2019-06-12 16:13:14 -0400

    Removes pointless use of #singleton_class

--- lib/subreddit_scraper/cli.rb
+++ lib/subreddit_scraper/cli.rb
@@ -2,7 +2,7 @@ module SubredditScraper
   class Cli
     extend Menu::Actions
     def self.before # used to append methods
-      (self.singleton_class.instance_methods - self.superclass.singleton_class.instance_methods).each do |name|
+      (self.methods - self.superclass.methods).each do |name|
         next if name == :before
         m = method(name)
         define_singleton_method(name) do |*args, &block|  
